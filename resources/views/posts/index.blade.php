abecf100141953d9766e854496e6d83a76edd675
darkside <darkside@localhost.localdomain>
2020-10-11 00:50:20 +0530

    changed posts to card style using flex-box

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,27 +1,30 @@
 @extends('layout.app')
 @section('content')
 
-<div class="container">
-    <h1>post</h1>
     <div class="container">
-        @if (count($posts) >= 1)
-            @foreach ($posts as $post)
-                <div class="card text-white bg-dark border-light z-depth-5 rounded-lg mb-3">
-                    <div class="container">
-                        <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
-                        <small>Written on {{$post->created_at}}</small>
-                    </div>
-                </div>
-            @endforeach
-        <hr class="">
-        <div class="container">
-            {{ $posts->links("pagination::bootstrap-4") }}
-        </div>
-        @else
-            <p>No posts found</p>
-        @endif
+        <h1>post</h1>
     </div>
-</div>
-
+    <section class="cards-wrapper">
+            @if (count($posts) >= 1)
+                @foreach ($posts as $post)
+                    <div class="card-grid-space">
+                        <div class="card">
+                            <div class="container">
+                                <h1><a href="/posts/{{$post->id}}">{{$post->title}}</a></h1>
+                                <div class="card-img-overlay d-flex align-items-end flex-column">
+                                   <div class="card-text mt-auto">Written on {{$post->created_at}}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+    </section>
+                <div class="container">
+                    {{ $posts->links("pagination::bootstrap-4") }}
+                </div>
+            @else
+                <p>No posts found</p>
+            @endif
+        </div>
 @endsection
 
