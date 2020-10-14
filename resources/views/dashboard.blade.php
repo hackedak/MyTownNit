2ec4f5973c9daa10b0d4ff98f32aa36bdbdb9a90
darkside <darkside@localhost.localdomain>
2020-10-14 19:42:00 +0530

    added dashboard link to navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,13 +19,12 @@
                         </tr>
                         @foreach ($posts as $post)
                         <tr>
-                            <th>{{$post->title}}</th>
+                            <th><a href="/posts/{{$post->id}}" class="btn btn-default">{{$post->title}}</a></th>
                             <th><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></th>
                             <th></th>
                         </tr>
                         @endforeach
                     </table>
-
                 </div>
             </div>
         </div>
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -42,6 +42,9 @@
                     <li class="nav-item">
                     <a class="nav-link" href="/posts/create">Create Post</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/posts">Posts</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
